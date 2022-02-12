956e58ce0bea3a3f1f7bf139911d24b2c28ec467
Jesus <jesuszmendoza14@gmail.com>
2022-02-11 22:30:32 -0600

    add routes

--- routes.js
+++ routes.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+router.get('/data', (req, res, next) => {
+  res.send('GETTING DATA FROM SPORTSDATAIO');
 
-router.get('/users', (req, res, next) => {
-  console.log("you have navigated to 'users'");
-  res.send('respond with a sdf');
+  let sportsData;
+
+  const exResponse = axios({
+    method: 'get',
+    url: 'https://api.sportsdata.io/v3/nfl/odds/json/BettingMetadata',
+    headers: {
+      "Content-Type": "application/json",
+      "Ocp-Apim-Subscription-Key": key
+    },
+  })
+  .then(response => {
+    console.log(`statusCode: ${response.status}`);
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
