1a3c4de68bcdced313d08e9901e0c1b93dec18d2
Jesus Mendoza <jesuszmendoza14@gmail.com>
2022-02-11 22:27:49 -0600

    add dotenv

--- routes.js
+++ routes.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+  console.log("Home page accessed");
+  res.send("welcome to the Home page");
+});
+
 router.get('/users', (req, res, next) => {
-  console.log("you have navigated to 'users'");
+  console.log("Users page");
   res.send('respond with a sdf');
 });
 
