eac02fcb364226e00923423479b77b83abadeaa7
Jesus <jesuszmendoza14@gmail.com>
2022-02-14 18:39:00 -0600

    update server routes

--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-router.get('/data', (req, res, next) => {
+// SPORTS DATA
+
+router.get('/sports-data/data', (req, res, next) => {
   res.send('GETTING DATA FROM SPORTSDATAIO');
 
   axios({
@@ -10,7 +12,29 @@ router.get('/data', (req, res, next) => {
     url: 'https://api.sportsdata.io/v3/nfl/odds/json/BettingMetadata',
     headers: {
       "Content-Type": "application/json",
-      "Ocp-Apim-Subscription-Key": process.env.API_KEY
+      "Ocp-Apim-Subscription-Key": process.env.SPORTSDATA_API_KEY
+    },
+  })
+  .then(response => {
+    console.log(`statusCode: ${response.status}`);
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+// SHARP SPORTS
+
+router.get('/sharp-sports/books', (req, res, next) => {
+  res.send('SHARP SPORTS BOOKS');
+
+  axios({
+    method: 'get',
+    url: 'https://api.sportsdata.io/v3/nfl/odds/json/BettingMetadata',
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Token ${process.env.SHARPSPORTS_API_KEY}`
     },
   })
   .then(response => {
