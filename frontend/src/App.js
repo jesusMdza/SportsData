d061dd7411315b05a68f44f9dcb08f7dcf2e6275
Jesus <jesuszmendoza14@gmail.com>
2022-02-14 18:38:32 -0600

    update server routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
+  async function postContext(data = {}) {
+    const response = await fetch('https://api.sharpsports.io/v1/context', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Token ${process.env.SHARPSPORTS_API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    return response.json();
+  }
+  
+  function popupWindow(url, title, win, w, h) {
+      const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
+      const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
+      return win.open(url, title, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
+  }
+  
+  function onClickButton() {
+    postContext({internalId: process.env.TEST_INTERNAL_ID})
+      .then(data => popupWindow(`https://ui.sharpsports.io/link/${data.cid}`,'SharpSports',window,500,600))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API testing</h1>
+      <Button sx={{ background: 'gray', color: 'white', fontWeight: 'bold', marginRight: '1em' }}>Get Data</Button>
+      <Button sx={{ background: 'green', color: 'white', fontWeight: 'bold' }} onClick={onClickButton} variant="primary">SportsBook Link</Button>
     </div>
   );
 }
